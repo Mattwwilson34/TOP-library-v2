f1d1e588edb6095a7705921ad40f3e5884a1d8a9
Matt Wilson <mattwwilson34@gmail.com>
2022-07-30 10:02:50 -0400

    Add: read,id vars to createBook func
    Add read and id variables to createBook func and removed
    id and isbn from parameters in favor of using node crypto
    module to autogenerate these values on each book obj.

--- book.js
+++ book.js
@@ -1,13 +1,24 @@
+const crypto = require('crypto');
+
 const createBook = (
   _title = 'no title provided at creation',
   _author = 'no author provided at creation',
   _pages = 'number of pages not provided at creation',
   _datePublished = 'no publishing date provided at creation',
   _summary = 'no summary provided at creation',
-  _isbn = 'no ISBN provided at creation'
+  _read = false
 ) => {
+  const _id = crypto.randomUUID();
+  const _isbn = crypto.randomUUID();
+
   return {
     // Getters
+    getBook() {
+      return { _id, _title, _author, _pages, _datePublished, _summary, _isbn };
+    },
+    getId() {
+      return _id;
+    },
     getTitle() {
       return _title;
     },
@@ -25,6 +36,13 @@ const createBook = (
     },
     getIsbn() {
       return _isbn;
+    },
+    getRead() {
+      return _read;
+    },
+    // Setters
+    setRead() {
+      _read = !_read;
     },
   };
 };
